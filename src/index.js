7f16bc3a2d45423d576d55933ea9815562c5a603
Hala Elsaka <halaelsaka2@gmail.com>
2020-08-07 10:55:23 +0200

    wrap app into Provider and connect store of redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import App from "./Containers/App";
 import "./Style/style.css"
 import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import store from "../src/Redux/store"
 
 const app = (
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.querySelector("#root"));
