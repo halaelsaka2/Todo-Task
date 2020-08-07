6a1f977f0b68dab2e0aed04aee0f8dfa20daf091
Hala Elsaka <halaelsaka2@gmail.com>
2020-08-07 10:57:43 +0200

    connect redux to Home Component and display all todos

--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
 
+import { connect } from "react-redux";
+
 import AddTodoList from "../../Components/AddTodoForm";
 import List from "../../Components/List";
+import { getAllToDos} from "../../Redux/actions/todosActions";
 
 class Home extends Component {
   state = {
   };
   componentDidMount() {
+    this.props.getAllTodos();
   }
   
+
   render() {
     return (
       <React.Fragment>
-        <AddTodoList />
-        <List />
+        <AddTodoList/>
+        <List toDoList={this.props.todos} />
       </React.Fragment>
     );
   }
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getAllTodos: () => dispatch(getAllToDos()),
+  };
+};
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos.todos,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
